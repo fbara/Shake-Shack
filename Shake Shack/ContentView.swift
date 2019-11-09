791ccc86ca8d02cde7d8dc4b5926980bc4f1d13a
Frank S. Bara <fbara1@yahoo.com>
2019-11-08 22:29:25 -0600

    Initial Commit

---
+++ Shake Shack/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shake Shack
+//
+//  Created by Frank Bara on 11/8/19.
+//  Copyright © 2019 BaraLabs. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
