c22a2a64209cec4e8e93f56766887748f70cf3e7
Frank S. Bara <fbara1@yahoo.com>
2019-11-09 19:27:35 -0600

    Completed adding animation.  End of part 1.

--- Shake Shack/WelcomeBottomButtonsView.swift
+++ Shake Shack/WelcomeBottomButtonsView.swift
@@ -9,8 +9,34 @@
 import SwiftUI
 
 struct WelcomeBottomButtonsView: View {
+    let buttons = [ButtonsWithImage(image: "clock", label: "History"),
+                    ButtonsWithImage(image: "bag", label: "Menu")]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        GeometryReader { geometry in
+            HStack {
+                ForEach(self.buttons, id: \.self) {button in
+                    Button(action: {
+                        print(button.label)
+                    }) {
+                        Image(systemName: button.image)
+                            .font(.title)
+                            .foregroundColor(.green)
+                        
+                        Text(button.label)
+                            .fontWeight(.heavy)
+                            .foregroundColor(.black)
+                    }
+                    .frame(width: geometry.size.width / 2.2, height: 60)
+                    .background(Color.white)
+                .cornerRadius(16)
+                    .shadow(color: (Color.black).opacity(0.3), radius: 4, x: 0, y: 0)
+                    .padding(.horizontal)
+                }
+            }
+        }
+        .frame(height: 60)
+    .padding()
     }
 }
 
@@ -18,4 +44,9 @@ struct WelcomeBottomButtonsView_Previews: PreviewProvider {
     static var previews: some View {
         WelcomeBottomButtonsView()
     }
+}
+
+struct ButtonsWithImage: Hashable {
+    let image: String
+    let label: String
 }
--- Shake Shack/WelcomeView.swift
+++ Shake Shack/WelcomeView.swift
@@ -9,6 +9,10 @@
 import SwiftUI
 
 struct WelcomeView: View {
+    @State private var isBottomViewOnScreen = false
+    @State private var isSignUpViewOnScreen = false
+    @State private var isMenuViewOnScreen = false
+    
     var body: some View {
         NavigationView {
             ZStack {
@@ -16,14 +20,34 @@ struct WelcomeView: View {
                 
                 VStack {
                     MenuButtonView()
+                        .offset(y: isMenuViewOnScreen ? 0 : -400)
+                        .animation(.spring(dampingFraction: 0.8))
+                        .onAppear {
+                            DispatchQueue.main.asyncAfter(deadline: .now() + 0.8) {
+                                self.isMenuViewOnScreen.toggle()
+                            }
+                        }
                     Spacer()
                     WelcomeTextView()
                     Spacer()
                     NavigationLink(destination: Text("Coming soon.")) {
                         SignInOrCreateButtonView()
                     }
+                    .offset(y: isSignUpViewOnScreen ? 0 : 400)
+                    .animation(.spring(dampingFraction: 0.6))
+                    .onAppear {
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 0.4) {
+                            self.isSignUpViewOnScreen.toggle()
+                        }
+                    }
                     
                     WelcomeBottomView()
+                    .offset(y: isBottomViewOnScreen ? 0 : 400)
+                    .animation(.spring(dampingFraction: 0.4))
+                    .onAppear {
+                            self.isBottomViewOnScreen.toggle()
+                    }
+                    
                 }
                 .frame(width: UIScreen.main.bounds.width)
                 .edgesIgnoringSafeArea(.bottom)
