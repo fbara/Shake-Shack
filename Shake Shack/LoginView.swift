b2812e32316d6f87db25aa3f26d51e6f03d8fe2d
Frank S. Bara <fbara1@yahoo.com>
2019-11-19 13:11:35 -0600

    Added Login View.  Moved files into groups.

---
+++ Shake Shack/LoginView.swift
@@ -0,0 +1,21 @@
+//
+//  LoginView.swift
+//  Shake Shack
+//
+//  Created by Frank Bara on 11/19/19.
+//  Copyright © 2019 BaraLabs. All rights reserved.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
